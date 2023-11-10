6058e61c22ad24ce9179ec7e19b6b173e687a3f9
UniversalOpen <147580391+UniversalOpen@users.noreply.github.com>
2023-11-10 13:24:54 +0700

    Update InteropSettings.cs

--- Shadowsocks.Interop/Settings/InteropSettings.cs
+++ Shadowsocks.Interop/Settings/InteropSettings.cs
@@ -8,7 +8,7 @@ namespace Shadowsocks.Interop.Settings
 {
     public class InteropSettings
     {
-        public string SsRustPath { get; set; }
+        public string SsRustPath { get; set; get; }
         public string V2RayCorePath { get; set; }
 
         public InteropSettings()
