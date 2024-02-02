5407cafb3fe1fa60a809906a9b60ddb74ee41d57
Uni <89723323+UniRevendBypass@users.noreply.github.com>
2024-02-02 09:26:51 +0700

    Update ConfigConverter.cs

--- Shadowsocks.CLI/ConfigConverter.cs
+++ Shadowsocks.CLI/ConfigConverter.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;;  
 using System.Linq;
-using System.Net.Http;
+using System.Net.Http; 
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading;
